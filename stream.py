0c589451d15815bbc5096cef8db5e4ea72b9ec67
RameezShuhaib <rameezshuhaib96@gmail.com>
2020-05-11 02:00:34 +0530

    resolved bug in Batch Operator

--- stream.py
+++ stream.py
@@ -1,21 +1,24 @@
 import itertools
+from copy import deepcopy
 from typing import List, Set, Callable, Iterator, Iterable, Union, Generator, Any
+
+from schedulers import Scheduler, SerialScheduler
 
 
 class Stream:
     def __init__(self, items: Union[Iterator, Iterable, Generator]):
         self._items = iter(items)
 
-    def map(self, func: Callable, scheduler):
-        return _MapOperator(func, scheduler, self)
+    def map(self, func: Callable, scheduler: "Scheduler" = None) -> "Stream":
+        return _MapOperator(func, self, scheduler)
 
-    def filter(self, func: Callable):
-        return Stream(filter(func, self))
+    def filter(self, func: Callable) -> "Stream":
+        return Stream(filter(deepcopy(func), self))
 
-    def take(self, count: int) -> Iterator:
+    def take(self, count: int) -> "Stream":
         return Stream(itertools.islice(self, count))
 
-    def batch(self, count: int) -> Iterator:
+    def batch(self, count: int) -> "Stream":
         return _BatchOperator(count, self)
 
     def concat(self):
@@ -27,7 +30,7 @@ class Stream:
     def set(self) -> Set:
         return set(self)
 
-    def __iter__(self) -> Iterator:
+    def __iter__(self):
         return self._items
 
     def __next__(self) -> Any:
@@ -49,19 +52,21 @@ class _ConcatOperator(Stream):
 
 
 class _MapOperator(Stream):
-    def __init__(self, func: Callable, scheduler, parent: "Stream") -> None:
+    def __init__(
+        self, func: Callable, parent: "Stream", scheduler: "Scheduler"
+    ) -> None:
         super().__init__(parent)
         self._parent = parent
-        self._scheduler = scheduler
+        self._scheduler = scheduler or SerialScheduler()
         self._func = func
 
-    def take(self, count):
+    def take(self, count) -> "Stream":
         for item in self._parent.take(count):
-            self._scheduler.add_task(self._func, item)
+            self._scheduler.add_task(deepcopy(self._func), item)
         return Stream(self._scheduler.results())
 
     def __iter__(self):
-        return map(self._func, self._parent)
+        return map(deepcopy(self._func), self._parent)
 
 
 class _BatchOperator(Stream):
@@ -70,9 +75,12 @@ class _BatchOperator(Stream):
         self._count = count
         self._parent = parent
 
-    def take(self, count: int) -> Iterator:
+    def take(self, count: int) -> "Stream":
         return Stream(itertools.islice(self, count))
 
     def __iter__(self):
         while True:
-            yield self._parent.take(self._count)
+            batch = self._parent.take(self._count).list()
+            if not batch:
+                return
+            yield iter(batch)
